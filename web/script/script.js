2d0dd394b5142e0f97994630fe7fb4211e2ad98d
SarG24 <sartaj2004@outlook.com>
2024-02-20 12:25:41 -0500

    chnages

--- web/script/script.js
+++ web/script/script.js
@@ -1,4 +1,4 @@
-
+// Buttons and Contents Variables
 const aboutUsButton = document.getElementById('about-us-button');
 const YssLiveButton = document.getElementById('yss-live-button');
 const aboutUsContent = document.getElementById('about-content');
@@ -11,6 +11,28 @@ const MetaNarContent = document.getElementById('meta-narrative-content');
 const ComDevContent = document.getElementById("community-development-content");
 const StartContent = document.getElementById("start-content");
 const MarketplaceContent = document.getElementById("marketplace-content");
+const FaqButton = document.getElementById("faq-button");
+const FaqContent = document.getElementById("faq");
+const SponsorButton = document.getElementById("sponsor-button");
+const SponsorContent = document.getElementById("sponsor-page");
+
+let logoText = document.querySelector(".logo");
+let audio = document.createElement("audio");
+audio.src = "/media/sounds/ui_chime.mp3";
+document.body.appendChild(audio);
+
+logoText.onmouseover = () => {
+    let resp = audio.play();
+    if (resp!== undefined) {
+        resp.then(_ => {
+            // autoplay starts!
+        }).catch(error => {
+            //show error
+        });
+    }
+}
+
+// Button Functionalities for road map project selector
 aboutUsButton.addEventListener('click', ()=> {
     aboutUsContent.style.display = "block";
     document.getElementById('about-content').scrollIntoView({ behavior: 'smooth' });
@@ -18,9 +40,10 @@ aboutUsButton.addEventListener('click', ()=> {
     MetaNarContent.style.display = "none";
     ComDevContent.style.display = "none";
     StartContent.style.display = "none";
-    MarketplaceContent.style.display = "none";
-});
+    FaqContent.style.display = "none";
+    SponsorContent.style.display = "none";
 
+});
 YssLiveButton.addEventListener('click', ()=> {
     aboutUsContent.style.display = "none";
     YssLiveContent.style.display = "block";
@@ -28,8 +51,8 @@ YssLiveButton.addEventListener('click', ()=> {
     MetaNarContent.style.display = "none";
     ComDevContent.style.display = "none";
     StartContent.style.display = "none";
-    MarketplaceContent.style.display = "none";
-
+    FaqContent.style.display = "none";
+    SponsorContent.style.display = "none";
 });
 MetaNarButton.addEventListener('click', ()=> {
     aboutUsContent.style.display = "none";
@@ -38,8 +61,8 @@ MetaNarButton.addEventListener('click', ()=> {
     document.getElementById('meta-narrative-content').scrollIntoView({ behavior: 'smooth' });
     ComDevContent.style.display = "none";
     StartContent.style.display = "none";
-    MarketplaceContent.style.display = "none";
-
+    FaqContent.style.display = "none";
+    SponsorContent.style.display = "none";
 });
 ComDevButton.addEventListener('click', ()=> {
     aboutUsContent.style.display = "none";
@@ -48,7 +71,8 @@ ComDevButton.addEventListener('click', ()=> {
     ComDevContent.style.display = "block";
     document.getElementById('community-development-content').scrollIntoView({ behavior: 'smooth' });
     StartContent.style.display = "none";
-    MarketplaceContent.style.display = "none";
+    FaqContent.style.display = "none";
+    SponsorContent.style.display = "none";
 });
 StartButton.addEventListener('click', ()=> {
     aboutUsContent.style.display = "none";
@@ -57,28 +81,43 @@ StartButton.addEventListener('click', ()=> {
     ComDevContent.style.display = "none";
     StartContent.style.display = "block";
     document.getElementById('start-content').scrollIntoView({ behavior: 'smooth' });
-    MarketplaceContent.style.display = "none";
+    FaqContent.style.display = "none";
+    SponsorContent.style.display = "none";
 });
-MarketplaceButton.addEventListener('click', ()=> {
+
+FaqButton.addEventListener('click', ()=> {
     aboutUsContent.style.display = "none";
     YssLiveContent.style.display = "none";
     MetaNarContent.style.display = "none";
     ComDevContent.style.display = "none";
     StartContent.style.display = "none";
-    MarketplaceContent.style.display = "block";
-    document.getElementById('marketplace-content').scrollIntoView({ behavior: 'smooth' });
+    FaqContent.style.display = "block";
+    SponsorContent.style.display = "none";
+    document.getElementById('faq').scrollIntoView({ behavior: 'smooth' });
+});
+SponsorButton.addEventListener('click', ()=> {
+    aboutUsContent.style.display = "none";
+    YssLiveContent.style.display = "none";
+    MetaNarContent.style.display = "none";
+    ComDevContent.style.display = "none";
+    StartContent.style.display = "none";
+    FaqContent.style.display = "none";
+    SponsorContent.style.display = "block";
+    document.getElementById('sponsor-page').scrollIntoView({ behavior: 'smooth' });
 });
 
+
+// Functionalities for Road Map Node Selectors Per Project
+
+// For About Us
 const overlay = document.getElementById('about-content-1-overlay');
 function handleClickAbout(buttonId, contentId) {
     const button = document.getElementById(buttonId);
     const content = document.getElementById(contentId);
-
     button.addEventListener('click', () => {
         content.style.display = content.style.display === 'none' ? 'block' : 'none';
         overlay.style.display = overlay.style.display === 'none' ? 'block' : 'none';
     });
-
     overlay.addEventListener('click', () => {
         content.style.display = 'none';
         overlay.style.display = 'none';
@@ -88,76 +127,70 @@ for (let i = 1; i <= 6; i++) {
     handleClickAbout(`about-content-${i}`, `about-content-${i}-opened`);
 }
 
+// For YSS Live
 const yslcoverlay = document.getElementById('yslc-content-overlay');
 function handleClickYslc(buttonId, contentId) {
     const button = document.getElementById(buttonId);
     const content = document.getElementById(contentId);
-
     button.addEventListener('click', () => {
         content.style.display = content.style.display === 'none' ? 'block' : 'none';
         yslcoverlay.style.display = yslcoverlay.style.display === 'none' ? 'block' : 'none';
     });
-
     yslcoverlay.addEventListener('click', () => {
         content.style.display = 'none';
         yslcoverlay.style.display = 'none';
     });
 }
-for (let i = 1; i <= 6; i++) {
+for (let i = 1; i <= 3; i++) {
     handleClickYslc(`yslc-content-${i}`, `yslc-content-${i}-opened`);
 }
 
-
-
+// For Meta Narrative
 const metacoverlay = document.getElementById('meta-content-overlay');
 function handleClickMeta(buttonId, contentId) {
     const button = document.getElementById(buttonId);
     const content = document.getElementById(contentId);
-
     button.addEventListener('click', () => {
         content.style.display = content.style.display === 'none' ? 'block' : 'none';
         metacoverlay.style.display = metacoverlay.style.display === 'none' ? 'block' : 'none';
     });
-
     metacoverlay.addEventListener('click', () => {
         content.style.display = 'none';
         metacoverlay.style.display = 'none';
     });
 }
-for (let i = 1; i <= 6; i++) {
+for (let i = 1; i <= 5; i++) {
     handleClickMeta(`meta-content-${i}`, `meta-content-${i}-opened`);
 }
 
-
+// For Community Development
 const comdevoverlay = document.getElementById('comdev-content-overlay');
 function handleClickComDev(buttonId, contentId) {
     const button = document.getElementById(buttonId);
     const content = document.getElementById(contentId);
-
     button.addEventListener('click', () => {
         content.style.display = content.style.display === 'none' ? 'block' : 'none';
         comdevoverlay.style.display = comdevoverlay.style.display === 'none' ? 'block' : 'none';
     });
-
     comdevoverlay.addEventListener('click', () => {
         content.style.display = 'none';
         comdevoverlay.style.display = 'none';
     });
 }
-for (let i = 1; i <= 6; i++) {
+// Change increment ie: i <= x for how many nodes exist in the road map (x)
+for (let i = 1; i <= 2; i++) {
     handleClickComDev(`comdev-content-${i}`, `comdev-content-${i}-opened`);
 }
 
+// For S.T.A.R.T
 const startoverlay = document.getElementById('start-content-overlay');
 function handleClickStart(buttonId, contentId) {
     const button = document.getElementById(buttonId);
     const content = document.getElementById(contentId);
-
     button.addEventListener('click', () => {
         content.style.display = content.style.display === 'none' ? 'block' : 'none';
         startoverlay.style.display = startoverlay.style.display === 'none' ? 'block' : 'none';
     });
-
     startoverlay.addEventListener('click', () => {
         content.style.display = 'none';
         startoverlay.style.display = 'none';
@@ -165,24 +198,4 @@ function handleClickStart(buttonId, contentId) {
 }
 for (let i = 1; i <= 6; i++) {
     handleClickStart(`start-content-${i}`, `start-content-${i}-opened`);
-}
-
-
-const mpoverlay = document.getElementById('mp-content-overlay');
-function handleClickMp(buttonId, contentId) {
-    const button = document.getElementById(buttonId);
-    const content = document.getElementById(contentId);
-
-    button.addEventListener('click', () => {
-        content.style.display = content.style.display === 'none' ? 'block' : 'none';
-        mpoverlay.style.display = mpoverlay.style.display === 'none' ? 'block' : 'none';
-    });
-
-    mpoverlay.addEventListener('click', () => {
-        content.style.display = 'none';
-        mpoverlay.style.display = 'none';
-    });
-}
-for (let i = 1; i <= 6; i++) {
-    handleClickMp(`mp-content-${i}`, `mp-content-${i}-opened`);
 }
